4b811d82aa3ef98f29966eb4ad4157aa0f94743a
TheCodingStar02 <143983736+TheCodingStar02@users.noreply.github.com>
2023-09-04 12:48:41 +0700

    Update pertemuan 5.py

--- 8A_IKKI/Pertemuan 5 - 28 Agustus 2023/pertemuan 5.py
+++ 8A_IKKI/Pertemuan 5 - 28 Agustus 2023/pertemuan 5.py
@@ -5,9 +5,22 @@ Apabila suhu ruangan lebih besar dari 26 derajat celcius,
 maka tampilkan kalimat “Ruangan panas. Nyalakan AC”
 '''
 
-#if else suhu ruangan
+#if then suhu ruangan
 suhu=int(input("Masukkan Suhu Ruangan : "))
 if suhu>26:
     print("Ruangan Panas. Nyalakan AC")
+print("Program Selesai")
+
+'''
+Buat sebuah program untuk menampilkan kondisi ruangan berdasarkan suhu ruangan.
+Apabila suhu ruangan lebih besar dari 26 derajat celcius, maka tampilkan kalimat 
+“Ruangan panas. Nyalakan AC”. Apabila tidak, tampilkan kalimat “Matikan AC.”
+
+'''
+#if then else suhu ruangan
+suhuruang=int(input("Masukkan Suhu Ruangan : "))
+if suhuruang>26:
+    print("Ruangan Panas. Nyalakan AC")
 else:
-    print("Program Selesai")
+    print("Matikan AC.")
+print("Program Selesai")

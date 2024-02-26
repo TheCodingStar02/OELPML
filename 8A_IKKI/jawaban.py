e508415a32d22051455cbf2d4a02434d098f5bab
TheCodingStar02 <143983736+TheCodingStar02@users.noreply.github.com>
2024-02-26 12:55:38 +0700

    Add files via upload

---
+++ 8A_IKKI/jawaban.py
@@ -0,0 +1,40 @@
+for i in range(3):
+    input_password = input("Masukkan password: ")
+
+    # Validasi password
+    if input_password == "password123":
+        print("Device unlocked.")
+        break
+    else:
+        print("Password salah.")
+else:
+    print("Password salah sebanyak 3x. Device terblokir.")
+
+
+
+'''
+SOAL
+
+Tugas Mandiri:
+Menghitung Total dari 5 angka
+
+Terdapat sebuah perulangan for yang akan mengulang 5 kali. 
+Deklarasi variabel Total dan buatlah input variabel untuk menerima angka
+yang diberikan user. Masukan variabel tersebut kedalam variabel Total 
+untuk menghitung total angka yang dimasukan dalam setiap loop. 
+Program akan berhenti apabila user sudah memasukan angka sebanyak 5 kali. 
+Program akan diakhiri dengan menampilkan value dari variabel Total.
+Buatlah kode python untuk program diatas!
+'''
+
+total=0
+jumlah=int(input("Berapa jumlah angka? "))
+
+for count in range(1,jumlah+1): #1 2 3 4 5
+    angka=int(input(f"Masukkan angka ke-{count}: "))
+    total+=angka
+print("Total dari",jumlah,"angka =",total,".")
+print(f"Total dari {jumlah} angka = {total}.")
+
+
+
---
+++ 8A_IKKI/soal jumlah angka.py
@@ -0,0 +1,15 @@
+'''
+Program : Menghitung total dari jumlah angka yang ditentukan
+
+1. Terdapat sebuah perulangan for yang akan menghitung jumlah total dari angka yang akan user masukkan. 
+“Misal user masukkan 10, maka sistem akan mengeksekusi 10 angka tersebut.”
+
+2. Deklarasi variabel Total dan buatlah input variabel untuk menerima jumlah angka yang diberikan user. 
+3. Buat for loop range, dimana program akan menghitung jumlah angka yang telah dipilih user
+4. Buat input variabel untuk masukkan angka yang akan di hitung
+5. Proses Variabel total dengan Variabel no.4 (total+=angka)
+6. Program akan diakhiri dengan menampilkan value dari variabel Total.
+7. Program akan berhenti apabila angka yang ditentukan user sudah selesai di input.
+Buatlah kode python untuk program diatas!
+'''
+
